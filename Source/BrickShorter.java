88e42486fe3b98de0737d2e2063a9d032325a647
Nazım Gürkan Demir <n.gurkandemir@gmail.com>
2015-12-09 02:09:03 +0200

    Created initial BrickShorter code.
    Source code for Brick class with Shorter powerup.

---
+++ Source/BrickShorter.java
@@ -0,0 +1,35 @@
+public class BrickShorter extends Brick implements PowerUp
+{
+	private PowerUpImage img;
+
+	public BrickShorter(int x, int y, int dimX, int dimY, int c)
+	{
+		super(x, y, dimX, dimY, c);
+		setImg(new PowerUpImage(getX(), getY(), getClass().getResource("PaddleLength.png"), this));
+	}
+
+	@Override
+	public void executePowerUp(SinglePlayer parent)
+	{
+		parent.setPowerupMess("Shorter paddle");
+		parent.getPdl().adjustLength(parent.getPdl().getLength() - 1);
+		parent.setPwrupMessDuration(200);
+	}
+
+	@Override
+	public void dropPowerUp(SinglePlayer parent)
+	{
+		parent.powerUpImgs.add(img);
+	}
+
+	public PowerUpImage getImg()
+	{
+		return img;
+	}
+
+	public void setImg(PowerUpImage img)
+	{
+		this.img = img;
+	}
+
+}
